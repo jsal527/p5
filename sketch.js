e5641645a48fdd1b4d6a7aa5b0114d28d8859ba0
jsal527 <108934762+jsal527@users.noreply.github.com>
2022-07-13 15:49:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   capture = createCapture(VIDEO);
   capture.elt.id="video"
   capture.size(320,240);
-  capture.hide();
+  //capture.hide();
   p = createP("Scanning...")
   codeReader
   .listVideoInputDevices()
