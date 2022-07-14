b11e627f51af73d74d99407d74396edc9c4271c9
jsal527 <108934762+jsal527@users.noreply.github.com>
2022-07-14 19:14:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup() {
            }
         }
     });
-  capture.elt.setAttribute('playsinline',True);
+  capture.elt.setAttribute('playsinline','');
   capture.elt.id="video"
   capture.hide();
   capture.size(w, h);
