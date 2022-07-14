4f461925f8f3fa6656b5825e0bf67310de167fed
jsal527 <108934762+jsal527@users.noreply.github.com>
2022-07-14 17:05:21 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup() {
   codeReader
   .listVideoInputDevices()
   .then(videoInputDevices => {
-    const firstDeviceId = videoInputDevices[1].deviceId;
+    const firstDeviceId = videoInputDevices[0].deviceId;
     codeReader.decodeFromVideoDevice(firstDeviceId, 'video', (result, err) => {
     if (result) {
       // properly decoded qr code
