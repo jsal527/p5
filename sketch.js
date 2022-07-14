69359b11526201acdc16f06db38a07e25a35bc59
jsal527 <108934762+jsal527@users.noreply.github.com>
2022-07-14 17:17:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
             width: w,
             height: h,
 	    facingMode: {
-                   exact: "environment"
+                   exact: "User"
            }
         }
     });
