05c7f3461e2147cfb71aeaed3a3da16a000bda19
jsal527 <108934762+jsal527@users.noreply.github.com>
2022-07-14 22:18:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ let capture, graphics, p;
 const codeReader = new ZXing.BrowserMultiFormatReader();
 function setup() {
   createCanvas(240,440);
-  background(32);
+  //background(32);
   ///graphics = createGraphics(320, 240);
   
   capture = createCapture(VIDEO);
-  capture.elt.id="video"
+  capture.elt.id="video";
   capture.size(240,440);
   capture.hide();
 
